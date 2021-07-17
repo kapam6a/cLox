1ee55c8b4b833457f22b36b40206d2e8a2f25847
Алексей Якименко <ayakimenko@Aleksejs-MacBook-Air.local>
2021-07-17 13:23:06 +0700

    Added scanner

---
+++ cLox/compiler.h
@@ -0,0 +1,13 @@
+//
+//  compiler.h
+//  cLox
+//
+//  Created by Алексей Якименко on 15.07.2021.
+//
+
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+void compile(const char* source);
+
+#endif
